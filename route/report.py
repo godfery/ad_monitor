a456a335276cbb6effcc4ae5d7781eb34c8f4c95
godfery <157062357@qq.com>
2018-08-02 16:40:06 +0800

    view latest data

--- route/report.py
+++ route/report.py
@@ -3,8 +3,13 @@ from flask import render_template, redirect, url_for, request,flash,Response
 from config.manager import app
 # from models.models import db
 from models.report import Monitor,db
+from route.func import user_login_required
 import time
+from datetime import datetime
+def dateformat(value,format="%Y-%m-%d"):
+    return datetime.fromtimestamp(value).strftime(format)
 
+app.jinja_env.filters['dateformat']=dateformat
 @app.route('/report',methods=['GET','POST'])
 def report():
     # print(request.values)
@@ -22,3 +27,16 @@ def report():
     db.session.commit()
 
     return Response('success')
+
+# 文章列表
+@app.route('/report/list/<int:page>/', methods=['GET'])
+@user_login_required
+def report_list(page):
+    if page is None:
+        page = 1
+    
+    page_data = Monitor.query.order_by(
+        Monitor.add_time.desc()
+    ).paginate(page=page, per_page=10)
+    
+    return render_template('report_list.html', title=u"模版列表", page_data=page_data)
